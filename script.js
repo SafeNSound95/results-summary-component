3c727911e0a264acb6fbed0f56fa210a18cc48a2
Saif Shalan <safensound95@gmail.com>
2024-06-29 06:44:09 +0300

    did some light work and preperations on html/css/js

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+// const btn = document.querySelector("button");
+
+// for (let i = 0; i < 2; i++) {
+//   const html = `<p>${i}</p>`;
+//   btn.insertAdjacentHTML("beforebegin", html);
+// }
