d5625b5fd9e7225c6924dcb46ef2a84cb89504aa
Saif Shalan <safensound95@gmail.com>
2024-06-30 04:22:55 +0300

    practically done with the mobile layout, did heavy work especially on the css/js files, just need to do queries for tablet/desktop and refactor

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 "use strict";
-// const btn = document.querySelector("button");
+const continueBtn = document.querySelector(".continue-btn");
 
-// for (let i = 0; i < 2; i++) {
-//   const html = `<p>${i}</p>`;
-//   btn.insertAdjacentHTML("beforebegin", html);
-// }
+let categoryData;
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      const categoryName = data[i].category.toLowerCase();
+      const html = `<div class="category category-${categoryName}">
+                      <div class="category-info">
+                        <img src= ${data[i].icon}>
+                        <span class="category-name category-name_${categoryName}">${data[i].category}</span>
+                      </div> 
+
+                      <div class="category-data">
+                        <p class="category-score">${data[i].score} / <span>100</span>
+                        </p>
+                      </div>
+                   </div>`;
+      continueBtn.insertAdjacentHTML("beforebegin", html);
+    }
+  });
+
+/*
+1] refactor.
+*/
