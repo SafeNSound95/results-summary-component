0f9fba883c366c912c0cb9753eee45ea4155acbd
Saif Shalan <safensound95@gmail.com>
2024-07-02 08:18:43 +0300

    did the desktop layout and styles then refactored everything. next up is final touches.

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 "use strict";
 const continueBtn = document.querySelector(".continue-btn");
 
-let categoryData;
+function insertData(data) {
+  for (let i = 0; i < data.length; i++) {
+    const categoryName = data[i].category.toLowerCase();
+    const html = `<div class="category category-${categoryName}">
+                    <div class="category-info">
+                      <img src= ${data[i].icon}>
+                      <span class="category-name category-name_${categoryName}">${data[i].category}</span>
+                    </div> 
+
+                    <div class="category-data">
+                      <p class="category-score">${data[i].score} <span> / 100</span>
+                      </p>
+                    </div>
+                 </div>`;
+    continueBtn.insertAdjacentHTML("beforebegin", html);
+  }
+}
+
 fetch("data.json")
   .then((res) => res.json())
-  .then((data) => {
-    for (let i = 0; i < data.length; i++) {
-      const categoryName = data[i].category.toLowerCase();
-      const html = `<div class="category category-${categoryName}">
-                      <div class="category-info">
-                        <img src= ${data[i].icon}>
-                        <span class="category-name category-name_${categoryName}">${data[i].category}</span>
-                      </div> 
-
-                      <div class="category-data">
-                        <p class="category-score">${data[i].score} / <span>100</span>
-                        </p>
-                      </div>
-                   </div>`;
-      continueBtn.insertAdjacentHTML("beforebegin", html);
-    }
-  });
-
-/*
-1] refactor.
-*/
+  .then((data) => insertData(data));
